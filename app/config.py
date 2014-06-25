6ab4e2cf68816bb534785a76b2c01c5673fcd0a6
Matteo Landi <matteo@matteolandi.net>
2014-06-26 00:35:17 +0200

    after fork repo cleanup

--- app/config.py
+++ app/config.py
@@ -10,12 +10,6 @@ LOGGER_NAME = APP_NAME
 LOG_ENABLE = True
 LOG_FORMAT = '[%(process)d] %(levelname)s %(message)s [in %(pathname)s:%(lineno)d]'
 
-DATABASE_URL = 'sqlite:///appdb.sqlite'
-
-TITANIUM_KEY = 'XXX'
-TITANIUM_LOGIN = 'XXX'
-TITANIUM_PASSWORD = 'XXX'
-
 DISABLE_HTTP_ACCEPT_CHECK = False
 
 
--- app/forms/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from web import form
-
-required = form.Validator('required', bool)
-integer = form.Validator('integer_required', bool)
-
