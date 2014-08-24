e2d42b6abda95b92fd54cc05764fa2f427a4ccfd
Matteo Landi <matteo@matteolandi.net>
2014-08-24 13:56:44 +0200

    Better i18n management -- now different controllers for different languages

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -12,8 +12,20 @@ class IndexController(object):
     def GET(self):
         lang = web.ctx.environ.get('HTTP_ACCEPT_LANGUAGE')
         lang = lang.split(',')[0] if lang else 'en-US'
+        lang = lang.split('-')[0]
+        raise web.seeother(lang)
+
+class IndexENController(object):
+    def GET(self):
         return web.ctx.render.index(gettext=partial(web.ctx.gettext,
-                                                    lang=lang))
+                                                    lang='en'))
+
+
+class IndexITController(object):
+    def GET(self):
+        return web.ctx.render.index(gettext=partial(web.ctx.gettext,
+                                                    lang='it'))
+
 
 class OldIndexController(object):
     def GET(self):
--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from app.controllers import IndexController
+from app.controllers import IndexENController
+from app.controllers import IndexITController
 from app.controllers import OldIndexController
 from app.controllers import TOSController
 
 
 URLS = (
     '/', IndexController,
+    '/en', IndexENController,
+    '/it', IndexITController,
     '/old', OldIndexController,
     '/tos', TOSController,
 )
