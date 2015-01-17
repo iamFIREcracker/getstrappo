198d707c09671cbe6e9d7843a6b84bef4d4a2aa6
Matteo Landi <matteo@matteolandi.net>
2015-01-17 16:23:27 +0100

    Remove session management

--- app/__init__.py
+++ app/__init__.py
@@ -27,24 +27,17 @@ def app_factory():
     from weblib.app_processors import load_gettext
     from weblib.app_processors import load_logger
     from weblib.app_processors import load_path_url
-    from weblib.app_processors import load_redis
     from weblib.app_processors import load_render
-    from weblib.app_processors import load_session
-    from weblib.session import RedisStore
 
     from app.urls import URLS
 
-    redis = weblib.redis.create_redis()
     views = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'views')
     app = web.application(URLS, globals())
-    session = web.session.Session(app, RedisStore(redis))
     gettext = weblib.gettext.create_gettext()
 
     app.add_processor(web.loadhook(load_logger))
     app.add_processor(web.loadhook(load_path_url))
     app.add_processor(web.loadhook(load_render(views)))
-    app.add_processor(web.loadhook(load_session(session)))
     app.add_processor(web.loadhook(load_gettext(gettext)))
-    app.add_processor(web.loadhook(load_redis(redis)))
 
     return app
